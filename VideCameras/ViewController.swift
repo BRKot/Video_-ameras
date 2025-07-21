c2d742e67c1d07f912301a7bd4219409d7757c31
BRKot <65824266+BRKot@users.noreply.github.com>
2025-07-21 07:46:28 +0500

    Первоначальный коммит проекта

---
+++ VideCameras/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  VideCameras
+//
+//  Created by Databriz on 21/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
